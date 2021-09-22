47e2b502d39dafa7146d4eafa5bf38879419d806
Magin Suarez <magin.suarez@eggeducacion.com>
2021-09-22 10:56:08 -0300

    Agrego mejoras

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,26 +1,43 @@
 
-import React from 'react'
+import React, {  useState  } from 'react'
 import { Button } from 'react-bootstrap';
 import '../styles/BodyComponent.css'
 import ListaAvatar from './ListaAvatar'
+
 const BodyComponent = () => {
 
-    /////////////////////////////////////
-    const bolsa = [1, 2, 3, 4, 5, 6, 7, 8];
+    const bolsa = [1, 2];
+    
+    //Memorizar
+    const [mostrar, setMostrar] = useState(true);    
 
+    const quieroMostrarLosBotones = () => {        
+        setMostrar(!mostrar);        
+    }
+
+    /////////////////////////////////////
     return (
         <div className="body_principal">
-            <Button variant="success" className="primary_button">Primary</Button>
-            {
-                bolsa.map((lluvia) => {
-                    return (
-                        <Button variant="success" className="primary_button_2">
-                            BOTON DE PRUEBA {lluvia}
-                        </Button>
-                    )
-                })
+            {   mostrar &&
+                <>
+                    <Button variant="success" className="primary_button">Primary</Button>
+                    {
+                        bolsa.map((lluvia) => {
+                            return (
+                                <Button variant="success" className="primary_button_2">
+                                    BOTON DE PRUEBA {lluvia}
+                                </Button>
+                            )
+                        })
+                    }
+                    <ListaAvatar />
+                </>                    
             }
-            <ListaAvatar />
+            <Button 
+                variant="success" 
+                className="primary_button"
+                onClick={() => quieroMostrarLosBotones() }
+            >{ mostrar ? 'Ocultar' : 'Mostrar' }</Button>
         </div>
 
     )
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,16 +4,16 @@ import '../styles/FooterComponent.css'
 const FooterComponent = () => {
     return (
         <div className='footer_principal'>
-       <footer>
-            <h4 className='titulo_footer'>
-                CELMAG SRL.
-            </h4>
-            <p>
-                <a href='htps://instagram.com'>Politicas</a>&nbsp;
-                <a href='https://instagram.com'>Privacidad</a>&nbsp;
-                <a href='https://instagram.com'>Condiciones</a>
-            </p>
-       </footer>
+            <footer>
+                    <h4 className='titulo_footer'>
+                        CELMAG SRL.
+                    </h4>
+                    <p>
+                        <a href='htps://instagram.com'>Politicas</a>&nbsp;
+                        <a href='https://instagram.com'>Privacidad</a>&nbsp;
+                        <a href='https://instagram.com'>Condiciones</a>
+                    </p>
+            </footer>
        </div>
     );
 }
