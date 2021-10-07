b22dd596b3d71045e8f42248adaaec0ef1a2d537
Magin Suarez <magin.suarez@eggeducacion.com>
2021-10-07 17:23:50 -0300

    Agrego login

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,46 @@
 
-import React from 'react'
-
+import React, { useState } from 'react'
 const HomeComponent = () => {        
-    return (
-        <>            
-            Hola este es mi Home
-        </>
+
+    const [ email, setEmail ] = useState('albertomaginsuarez@gmail.com');
+    const [ password, setPassword ] = useState('');
+    
+    const cambiarEmail = (event) => {        
+        setEmail(event.target.value)        
+    }
+    
+    const cambiarPassword = (event) => {        
+        setPassword(event.target.value)  
+        console.log(password)      
+    }
+
+    const validarMyEmailAndPassword = () => {                
+        console.log(email)        
+        console.log(password)
+        alert("La contraseña del usuario " + email+ " es incorrecto")        
+    }    
+
+    return (        
+        <div>            
+            <input type="text" value={email} onChange={(event) => cambiarEmail(event)}/>
+            <input type="password" value={password} onChange={(event) => cambiarPassword(event)}/>
+            <button type="button" onClick={ () => validarMyEmailAndPassword() }>Iniciar sesión</button>
+            <a href="/resetPassword">¿Olvidaste tu contraseña?</a>
+            <hr/>
+            <button>Crear cuenta nueva</button>
+        </div>
     )
 }
 
 export default HomeComponent
+
+/** 
+<form method="POST" action="https://prueba.api.com">            
+    <input type="text"/>
+    <input type="password"/>
+        <button>Iniciar sesión</button>
+        <a href="/resetPassword">¿Olvidaste tu contraseña?</a>
+        <hr/>
+    <button type="submit">Crear cuenta nueva</button>
+</form>
+*/
