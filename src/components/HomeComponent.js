93d4f50c56160a48f5af9aafed891467dfeb4d71
Magin Suarez <magin.suarez@eggeducacion.com>
2021-10-21 17:03:44 -0300

    Agrego mejoras

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,32 +4,41 @@ import '../styles/LoginComponent.css'
 
 const HomeComponent = () => {        
 
-    const [ email, setEmail ] = useState('albertomaginsuarez@gmail.com');
+
+    const usuarioBase = "celina@gmail.com";
+    const contraseñaBase = "123456";
+
+    const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
-    
-    const cambiarEmail = (event) => {        
-        setEmail(event.target.value)        
-    }
-    
-    const cambiarPassword = (event) => {        
-        setPassword(event.target.value)  
-        console.log(password)      
+    const [ pais, setPais ] = useState('Argentina');        
+
+    const cambiarEmail = (event) => {                
+        setEmail(event.target.value);                
     }
 
-    const validarMyEmailAndPassword = () => {                
-        console.log(email)        
-        console.log(password)
-        alert("La contraseña del usuario " + email+ " es incorrecto")        
-    }    
+    const cambiarPassword = (event) => {        
+        setPassword(event.target.value);        
+    }
+
+    const validarMyEmailAndPassword = () => {                        
+        if( email === usuarioBase && contraseñaBase === password) {
+            alert("Login correcto");
+        } else {
+            alert("Email o la contraseña es incorrecta");
+        }
+    }        
 
     return (        
         <div className='login_principal'>            
-            <input className='mail_principal' type="text" value={email} onChange={(event) => cambiarEmail(event)}/>&nbsp;
-            <input className='password_principal' type="password" value={password} onChange={(event) => cambiarPassword(event)}/>&nbsp;
+            <input className='mail_principal' type="text" placeholder="Ingrese su email" value={email} onChange={(event) => cambiarEmail(event)}/>&nbsp;
+            <input className='password_principal' type="password" placeholder="Ingrese un password" value={password} onChange={(event) => cambiarPassword(event)}/>&nbsp;
             <button className='iniciar_sesion_principal' type="button" onClick={ () => validarMyEmailAndPassword() }>Iniciar sesión</button>&nbsp;
             <a className= 'olvidaste_contraseña' href="/resetPassword">¿Olvidaste tu contraseña?</a>
             <hr/>
-            <button className='crear_cuenta_principal'>Crear cuenta nueva</button>
+            {pais}            
+            <button onClick={() => setPais('Brasil')}>Voy a viajar</button>
+            {}
+            <button className='crear_cuenta_principal'>Crear cuenta nueva</button>            
         </div>
     )
 }
